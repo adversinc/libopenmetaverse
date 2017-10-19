a84645b5cf4b9138c63510220f43cc82598b3d77
Dino Fabrello <oleg@borodkin.ru>
2017-10-19 21:10:49 +0300

    Extended meshmerizer

--- OpenMetaverse.Rendering.Meshmerizer/MeshmerizerR.cs
+++ OpenMetaverse.Rendering.Meshmerizer/MeshmerizerR.cs
@@ -70,10 +70,7 @@ namespace OpenMetaverse.Rendering
             for (int i = 0; i < newPrim.coords.Count; i++)
             {
                 PrimMesher.Coord c = newPrim.coords[i];
-								
-								// Also saving the normal within the vertice
-								PrimMesher.Coord n = newPrim.normals[i];
-								mesh.Vertices.Add(new Vertex { Position = new Vector3(c.X, c.Y, c.Z), Normal = new Vector3(n.X, n.Y, n.Z) });
+								mesh.Vertices.Add(new Vertex { Position = new Vector3(c.X, c.Y, c.Z) });
 						}
 
             mesh.Indices = new List<ushort>(newPrim.faces.Count * 3);
@@ -87,6 +84,42 @@ namespace OpenMetaverse.Rendering
 
             return mesh;
         }
+
+				 /// <summary>
+        /// Generates a basic mesh structure from a primitive, adding normals data.
+        /// A 'SimpleMesh' is just the prim's overall shape with no material information.
+        /// </summary>
+        /// <param name="prim">Primitive to generate the mesh from</param>
+        /// <param name="lod">Level of detail to generate the mesh at</param>
+        /// <returns>The generated mesh or null on failure</returns>
+				public OMVR.SimpleMesh GenerateSimpleMesh2(OMV.Primitive prim, OMVR.DetailLevel lod) {
+					PrimMesher.PrimMesh newPrim = GeneratePrimMesh(prim, lod, true);
+					if(newPrim == null) 
+						return null;
+
+					SimpleMesh mesh = new SimpleMesh();
+					mesh.Path = new Path();
+					mesh.Prim = prim;
+					mesh.Profile = new Profile();
+					mesh.Vertices = new List<Vertex>(newPrim.coords.Count);
+					for(int i = 0; i < newPrim.coords.Count; i++) {
+						PrimMesher.Coord c = newPrim.coords[i];
+						// Also saving the normal within the vertice
+						PrimMesher.Coord n = newPrim.normals[i];
+						mesh.Vertices.Add(new Vertex { Position = new Vector3(c.X, c.Y, c.Z), Normal = new Vector3(n.X, n.Y, n.Z) });
+					}
+
+					mesh.Indices = new List<ushort>(newPrim.faces.Count * 3);
+					for(int i = 0; i < newPrim.faces.Count; i++) {
+						PrimMesher.Face face = newPrim.faces[i];
+						mesh.Indices.Add((ushort)face.v1);
+						mesh.Indices.Add((ushort)face.v2);
+						mesh.Indices.Add((ushort)face.v3);
+					}
+
+					return mesh;
+				}
+
 
         /// <summary>
         /// Generates a basic mesh structure from a sculpted primitive.
