e4c120f9a5349b8f0adc7d4d3edd038a8d4a0241
Dino Fabrello <dinofabrello@slbiz2life.com>
2020-05-27 11:40:33 -0700

    Add modern caps asset download

---
+++ OpenMetaverse/Assets/AssetTypes/AssetGeneric.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using OpenMetaverse;
+
+namespace OpenMetaverse.Assets
+{
+    /// <summary>
+    /// Represents an asset which type is not important for us
+    /// </summary>
+    public class AssetGeneric: Asset
+    {
+        /// <summary>Override the base classes AssetType</summary>
+        public override AssetType AssetType { get { return AssetType.Unknown; } }
+
+        /// <summary>Default Constructor</summary>
+        public AssetGeneric() { }
+
+        /// <summary>
+        /// Construct an Asset object
+        /// </summary>
+        /// <param name="assetID">A unique <see cref="UUID"/> specific to this asset</param>
+        /// <param name="assetData">A byte array containing the raw asset data</param>
+        public AssetGeneric(UUID assetID, byte[] assetData)
+            : base(assetID, assetData)
+        {
+        }
+
+        public override void Encode() { }
+        public override bool Decode() { return true; }
+    }
+}
